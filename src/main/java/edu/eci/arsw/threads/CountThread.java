66b35614d299f054982227182210712192ff56b6
Dav10-32 <david.palacios-p@mail.escuelaing.edu.co>
2026-01-27 18:49:31 -0500

    ARSW LAB 1: Punto 1_avanceClase

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,19 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread extends Thread{
+
+    private int first;
+    private int second;
+
+    public CountThread(int first, int second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public void run(){
+        for(int i=first; i<=second; i++){
+            System.out.println(i);
+        }
+    }
 }
